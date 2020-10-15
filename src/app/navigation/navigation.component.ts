0ed472a5bb08335dde5ba1e70ea7abef275b8c51
unknown <hjain9589@gmail.com>
2020-10-15 21:21:19 +0530

    9toys

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  logo="assets/images/9toyslogo.jpeg";
+  image1="assets/images/5b59f9705c5e5236008b46a5.jpg";
+  image2="assets/images/banner-3.png";
+  image3="assets/images/Toy-Store.jpg";
+  image4="assets/images/images.png";
+  image5="assets/images/correct.png";
+  image6="assets/images/697163_help_512x512.png";
+  image7="assets/images/download (9).jpg";
+  image8="assets/images/family-banner24.jpg";
+  image9="assets/images/Childrens-Day-wishes-in-English-Hindi.jpg";
+  image10="assets/images/917.jpg";
+  image11="assets/images/5e838b9c0c2a622acd76c3c8.jpg";
+  image12="assets/images/kids.jpg";
+  image13="assets/images/P1.jpg";
+  image14="assets/images/9toyslogo.jpeg";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
